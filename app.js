cc9d914066bc10db1aef58513bba7dd54cc947d4
Shankhadeep Bhadra <shankhadeepraj@gmail.com>
2019-12-15 15:41:51 +0530

    added templete engine

--- app.js
+++ app.js
@@ -1,26 +1,24 @@
-const express = require("express");
-const path = require("path");
-const bodyParser = require("body-parser");
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.set("view engine", "pug"); //to implement the view engine
-app.set("views", "views"); //to explicitly tell express where to implement the view engine
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
-const adminData = require("./routes/admin");
-const shopRouter = require("./routes/shop");
+const adminData = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use("/admin", adminData.routes);
-app.use(shopRouter);
+app.use('/admin', adminData.routes);
+app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).render("404");
+  res.status(404).render('404', { pageTitle: 'Page Not Found' });
 });
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Server is stared on port ${port}`);
-});
+app.listen(3000);
--- routes/shop.js
+++ routes/shop.js
@@ -1,14 +1,22 @@
-const express = require("express");
-const path = require("path");
+const path = require('path');
 
-const rootDir = require("../utils/path");
-const adminData = require("./admin");
+const express = require('express');
+
+const rootDir = require('../util/path');
+const adminData = require('./admin');
 
 const router = express.Router();
 
-router.get("/", (req, res, next) => {
+router.get('/', (req, res, next) => {
   const products = adminData.products;
-  res.render("shop", { prods: products, docTitle: "Shop" });
+  res.render('shop', {
+    prods: products,
+    pageTitle: 'Shop',
+    path: '/',
+    hasProducts: products.length > 0,
+    activeShop: true,
+    productCSS: true
+  });
 });
 
 module.exports = router;
